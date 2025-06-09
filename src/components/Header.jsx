81ced7475b5faa418c394f449657a69a8253d565
Vaporisateur <tupeuxpasteco@protonmail.com>
2025-06-09 14:08:39 +0200

    Add Argent Bank logo and tree image in WEBP format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../assets/img/argentBankLogo.png";
+import logo from "../assets/img/argentBankLogo.webp";
 import Button from "./Button";
 
 function Header({ isAuthenticated = false, userName = "", onLogout }) {
